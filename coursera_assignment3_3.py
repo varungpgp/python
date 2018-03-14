e450f958806b74b2a862405adde9fc89893e9f0e
Delta <varunpoojary11@gmail.com>
2018-03-15 00:56:57 +0530

    coursera_assignment3_3

---
+++ coursera_assignment3_3.py
@@ -0,0 +1,14 @@
+score = input("Enter Score: ")
+score1 = float(score)
+if score1 < 0.6:
+    print("F")
+elif score1 > 1.0:
+    print("Error! please enter number between 0 t0 1.0")
+elif score1 >= 0.9:
+    print("A")
+elif score1 >= 0.8:
+    print("B")
+elif score1 >= 0.7:
+    print("C")
+elif score1 >= 0.6:
+    print("D")
