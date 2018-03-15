7e404a1cc6c6b2a94501cbde69970ccc5b316d6d
Delta <varunpoojary11@gmail.com>
2018-03-15 18:18:13 +0530

    assignment3.1

--- coursera_assignment3_1.py
+++ coursera_assignment3_1.py
@@ -1,4 +1,5 @@
 #this code is for coursera python for everybody assignment 3.1
+#######################################################################3
 #Write a program to prompt the user for hours and rate per hour using
 #input to compute gross pay. Pay the hourly rate for the hours up to 40
 #and 1.5 times the hourly rate for all hours worked above 40 hours. Use 45
