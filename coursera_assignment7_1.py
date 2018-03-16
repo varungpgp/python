f6b94340747e5ed88ae6f42c30c1b442d62c5098
Delta <varunpoojary11@gmail.com>
2018-03-16 21:53:34 +0530

    assignment 7.1

---
+++ coursera_assignment7_1.py
@@ -0,0 +1,16 @@
+#this code is for python for everybody assignment 7.1
+###########################################################################
+#7.1 Write a program that prompts for a file name, then opens that
+#file and reads through the file, and print the contents of the file
+#in upper case. Use the file words.txt to produce the output below.
+#You can download the sample data at http://www.py4e.com/code3/words.txt
+
+
+
+# Use words.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+for line in fh:
+    vari = line.rstrip()
+    var = vari.upper()
+    print(var)
